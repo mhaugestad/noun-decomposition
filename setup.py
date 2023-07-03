b8b35cf4092af7b605541cab014d3e87c83c5897
Mathias Haugestad <mhaugestad@gmail.com>
2023-07-03 21:29:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mhaugestad/noun-decomposition",
-    packages=setuptools.find_packages(include=['.']),
+    packages=setuptools.find_packages(include=['.*', 'secos.*']),
     install_requires=['scipy', 'numpy', 'pytest', 'importlib-resources'],
     python_requires='>=3.6',
 )
